75fd72a22719538296533a4296554925fe229b6a
linzeguang <15817289730@163.com>
2025-07-13 16:51:49 +0800

    feat: add evn

--- src/constants/evnParams.ts
+++ src/constants/evnParams.ts
@@ -1,5 +1,12 @@
 export const ENV_PARAMS = {
   MODE: import.meta.env.VITE_MODE,
+  // api
   API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
-  API_TARGET_URL: import.meta.env.VITE_API_TARGET_URL
+  API_TARGET_URL: import.meta.env.VITE_API_TARGET_URL,
+  PHP_BASE_URL: import.meta.env.VITE_PHP_BASE_URL,
+  PHP_TARGET_URL: import.meta.env.VITE_PHP_TARGET_URL,
+  // chain
+  RPC: import.meta.env.VITE_RPC,
+  CHAIN_ID: import.meta.env.VITE_CHAIN_ID,
+  INFO_CONTRACT: import.meta.env.VITE_INFO_CONTRACT
 }
