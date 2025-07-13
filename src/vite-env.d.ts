7443f834aa2a753b6c27d3a8b795d52b1fc4469e
linzeguang <15817289730@163.com>
2025-07-13 16:52:12 +0800

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,6 +7,9 @@ interface ImportMetaEnv {
   readonly VITE_API_TARGET_URL: string
   readonly VITE_PHP_BASE_URL: string
   readonly VITE_PHP_TARGET_URL: string
+  readonly VITE_RPC: string
+  readonly VITE_CHAIN_ID: string
+  readonly VITE_INFO_CONTRACT: `0x${string}`
 }
 
 interface ImportMeta {
