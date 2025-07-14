1d5426b648b93454cb2e164c008ff81eee40bc98
linzeguang <15817289730@163.com>
2025-07-14 18:24:51 +0800

    Tokenomic

--- src/components/token/Tokenomic.tsx
+++ src/components/token/Tokenomic.tsx
@@ -5,23 +5,37 @@ import { ResponsivePie } from '@nivo/pie'
 import { Icon } from '@/components/svgr'
 import TokenAccordionItem from '@/components/token/TokenAccordionItem'
 import { AccordionRoot } from '@/components/ui/Accordion'
+import { Grid } from '@/components/ui/Box'
 import { HarmonyOSSansText } from '@/components/ui/Text'
+import { randomColor } from '@/lib/utils'
+
+const COLOR = {
+  LPHMining: randomColor(),
+  THRewards: randomColor(),
+  TSRewards: randomColor(),
+  Defi: randomColor()
+}
 
 const data = [
   {
-    id: 'presalc',
-    label: 'Presalc',
-    value: 40
+    id: 'LPHMining',
+    label: 'LPH Mining',
+    value: 65
   },
   {
-    id: 'liquidity',
-    label: 'Liquidity',
-    value: 30
-  },
-  {
-    id: 'burnt',
-    label: 'Burnt',
+    id: 'THRewards',
+    label: 'TH Rewards',
     value: 20
+  },
+  {
+    id: 'TSRewards',
+    label: 'TS Rewards',
+    value: 10
+  },
+  {
+    id: 'Defi',
+    label: 'Defi',
+    value: 5
   }
 ]
 
@@ -39,29 +53,29 @@ const Tokenomic: React.FC<{ className?: string; defaultValue?: string }> = (prop
           </>
         }
         content={
-          <div className="mx-auto aspect-square w-4/5 max-w-64">
-            <ResponsivePie /* or Pie for fixed dimensions */
-              data={data}
-              margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
-              innerRadius={0.7}
-              padAngle={0.6}
-              enableArcLabels={false}
-              enableArcLinkLabels={false}
-              isInteractive={false}
-              legends={[
-                {
-                  anchor: 'bottom',
-                  direction: 'row',
-                  translateY: 20,
-                  itemWidth: 56,
-                  itemHeight: 10,
-                  itemsSpacing: 12,
-                  symbolSize: 10,
-                  itemTextColor: '#fff'
-                }
-              ]}
-            />
-          </div>
+          <>
+            <div className="mx-auto aspect-square w-4/5 max-w-64">
+              <ResponsivePie /* or Pie for fixed dimensions */
+                data={data}
+                colors={(d) => COLOR[d.id as keyof typeof COLOR]}
+                margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+                innerRadius={0.7}
+                padAngle={0.6}
+                enableArcLabels={false}
+                enableArcLinkLabels={false}
+                isInteractive={false}
+                legends={[]}
+              />
+            </div>
+            <Grid className="mx-auto w-4/5 max-w-64 grid-cols-2 gap-0.5">
+              {data.map(({ id, label }) => (
+                <HarmonyOSSansText className="flex items-center gap-1 text-xs" variant="secondary">
+                  <div className="size-2" style={{ backgroundColor: COLOR[id as keyof typeof COLOR] }} />
+                  <span>{label}</span>
+                </HarmonyOSSansText>
+              ))}
+            </Grid>
+          </>
         }
       />
     </AccordionRoot>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,7 +10,7 @@ export function randomColor() {
   const r = Math.floor(Math.random() * 256)
   const g = Math.floor(Math.random() * 256)
   const b = Math.floor(Math.random() * 256)
-  return `rgba(${r},${g},${b},${0.3})`
+  return `rgba(${r},${g},${b},${1})`
 }
 
 export function copy(text: string) {
