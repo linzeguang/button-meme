3920ef3d7b9c514c7bf1df1ba493120a3032c699
linzeguang <15817289730@163.com>
2025-07-29 23:54:18 +0800

    proxy

---
+++ src/proxy/rpc.ts
@@ -0,0 +1,26 @@
+// api/rpc.ts
+export default async function handler(req: any, res: any) {
+  const targetUrl = 'http://ec2-54-251-227-86.ap-southeast-1.compute.amazonaws.com:6979' // 例如 Infura/Alchemy 自己的 RPC 地址
+
+  const response = await fetch(targetUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+      // 添加 Authorization 头如果你的 RPC 服务需要
+      // "Authorization": "Bearer YOUR_KEY"
+    },
+    body: JSON.stringify(req.body)
+  })
+
+  const data = await response.json()
+
+  res.setHeader('Access-Control-Allow-Origin', '*') // 或具体域名
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end()
+  }
+
+  res.status(response.status).json(data)
+}
